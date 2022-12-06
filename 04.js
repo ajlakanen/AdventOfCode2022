e50ccb40a666c9b27f84862948f5cee5b20b5923
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-06 12:34:00 +0200

    Documentation

--- 04.js
+++ 04.js
@@ -4,11 +4,20 @@ const sortNumbers = (a, b) => {
   return parseInt(a) - parseInt(b);
 };
 
-// Original data is in form (e.g.) 2-4,6-8
-// where a=2, b=4, c=6, d=8
+// Check whether a--b is fully contained in c--d or vice versa
+// Original data is in form (e.g.) 2-4,6-8 where a=2, b=4, c=6, d=8
+// ASSUMPTION! a <= c
 const isFullyContained = (a, b, c, d) => {
   if (parseInt(a) === parseInt(c)) return 1;
   if (parseInt(d) <= parseInt(b)) return 1;
+  return 0;
+};
+
+// Check whether a--b overlaps with c--d
+// Original data is in form (e.g.) 2-4,6-8 where a=2, b=4, c=6, d=8
+// ASSUMPTION! a <= c
+const overlap = (a, b, c, d) => {
+  if (parseInt(c) <= parseInt(b)) return 1;
   return 0;
 };
 
@@ -28,7 +37,7 @@ let assignments = lines
   .map((l) => l.split(","))
   .map((l) => l.sort(sortNumbers).map((a) => a.split("-")));
 
-//console.log(assignments);
+console.log(assignments);
 
 let sum = 0;
 assignments.forEach((element) => {
@@ -37,3 +46,11 @@ assignments.forEach((element) => {
 });
 
 console.log(sum);
+
+let sum2 = 0;
+assignments.forEach((element) => {
+  [[a, b], [c, d]] = element;
+  sum2 += overlap(a, b, c, d);
+});
+
+console.log(sum2);
