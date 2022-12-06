746686a1899bfe85105d91a556053e9475374111
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-06 20:35:42 +0200

    Day 6 part 2

--- 06.js
+++ 06.js
@@ -8,9 +8,9 @@ dataRaw.split(/\r?\n/).forEach((line) => {
   data = line;
 });
 
-const findMarker = (data) => {
-  for (let i = 3; i < data.length; i++) {
-    const str = data.slice(i - 3, i + 1);
+const findMarker = (data, width) => {
+  for (let i = width - 1; i < data.length; i++) {
+    const str = data.slice(i - (width - 1), i + 1);
     markerFound = true;
     for (let j = 0; j < str.length; j++) {
       for (let k = j + 1; k < str.length; k++) {
@@ -22,4 +22,6 @@ const findMarker = (data) => {
   }
 };
 
-console.log(findMarker(data));
+console.log(findMarker(data, 4));
+
+console.log(findMarker(data, 14));
