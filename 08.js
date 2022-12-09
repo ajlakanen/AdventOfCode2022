a845046c8cf1d304fce306f9ad9f890494efb125
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-09 10:43:44 +0200

    Day 8 part 1

--- 08.js
+++ 08.js
@@ -13,26 +13,15 @@ dataRaw.split(/\r?\n/).forEach((line) => {
 const isVisible = (array, row, col) => {
   const cols = array[0].length;
   // console.log(cols);
-  let left = true;
-  for (let ix = 0; ix < col; ix++) {
+  let left = (right = top = bottom = true);
+  for (let ix = 0; ix < col; ix++)
     if (array[row][ix] >= array[row][col]) left = false;
-  }
-
-  let right = true;
-  for (let ix = col + 1; ix < cols; ix++) {
+  for (let ix = col + 1; ix < cols; ix++)
     if (array[row][ix] >= array[row][col]) right = false;
-    //console.log("row", row, "col", col, "right", right);
-  }
-
-  let top = true;
-  for (let iy = 0; iy < row; iy++) {
+  for (let iy = 0; iy < row; iy++)
     if (array[iy][col] >= array[row][col]) top = false;
-  }
-
-  let bottom = true;
-  for (let iy = row + 1; iy < array.length; iy++) {
+  for (let iy = row + 1; iy < array.length; iy++)
     if (array[iy][col] >= array[row][col]) bottom = false;
-  }
 
   let visible = left || right || top || bottom;
   if (visible) {
