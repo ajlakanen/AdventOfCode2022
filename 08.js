54ccbeb1b37f1d1cc6e0b28de4c05d92d43f08af
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-09 17:44:29 +0200

    Day 8 part 2

--- 08.js
+++ 08.js
@@ -5,33 +5,66 @@ let matrix = [];
 // read the data
 const dataRaw = f.readFileSync("08-data.txt", "utf-8");
 dataRaw.split(/\r?\n/).forEach((line) => {
-  matrix.push(line);
+  l = [];
+  [...line].map((a) => l.push(parseInt(a)));
+  matrix.push(l);
 });
 
-//console.log(matrix);
-
 const isVisible = (array, row, col) => {
+  const rows = array.length;
   const cols = array[0].length;
   let left = (right = top = bottom = true);
-  for (let ix = 0; ix < col; ix++)
-    if (array[row][ix] >= array[row][col]) left = false;
-  for (let ix = col + 1; ix < cols; ix++)
-    if (array[row][ix] >= array[row][col]) right = false;
-  for (let iy = 0; iy < row; iy++)
-    if (array[iy][col] >= array[row][col]) top = false;
-  for (let iy = row + 1; iy < array.length; iy++)
-    if (array[iy][col] >= array[row][col]) bottom = false;
+
+  // left to right
+  let leftMinDist = col;
+  for (let ix = 0; ix < col; ix++) {
+    if (array[row][ix] >= array[row][col]) {
+      left = false;
+      leftMinDist = col - ix;
+    }
+  }
+
+  // right to left
+  let rightMinDist = cols - 1 - col;
+  for (let ix = cols; ix >= col + 1; ix--) {
+    if (array[row][ix] >= array[row][col]) {
+      right = false;
+      rightMinDist = ix - col;
+    }
+  }
+
+  // top to bottom
+  let topMinDist = row;
+  for (let iy = 0; iy < row; iy++) {
+    if (array[iy][col] >= array[row][col]) {
+      top = false;
+      topMinDist = row - iy;
+    }
+  }
+
+  // bottom to top
+  let bottomMinDist = rows - 1 - row;
+  for (let iy = rows - 1; iy >= row + 1; iy--) {
+    if (array[iy][col] >= array[row][col]) {
+      bottom = false;
+      bottomMinDist = iy - row;
+    }
+  }
 
   let visible = left || right || top || bottom;
-  if (visible) return true;
-  return false;
+  let scenicScore = leftMinDist * rightMinDist * topMinDist * bottomMinDist;
+  return { visible, scenicScore };
 };
 
 let visibles = 0;
+let maxScenicScore = 0;
 for (let iy = 1; iy < matrix.length - 1; iy++) {
   for (let ix = 1; ix < matrix.length - 1; ix++) {
-    if (isVisible(matrix, iy, ix)) visibles++;
+    const { visible, scenicScore } = isVisible(matrix, iy, ix);
+    if (visible) visibles++;
+    if (scenicScore > maxScenicScore) maxScenicScore = scenicScore;
   }
 }
 
 console.log(visibles + matrix.length * 2 + matrix[0].length * 2 - 4);
+console.log(maxScenicScore);
