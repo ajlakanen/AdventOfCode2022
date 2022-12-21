94cef0a01b2b6b8e28e0b6206ac7f7ebb05abe53
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-21 23:05:02 +0200

    Day 11 part 2 WIP, BigInts not working

--- 11.js
+++ 11.js
@@ -9,26 +9,37 @@ dataRaw.split(/\r?\n/).forEach((line) => {
 });
 
 const sortDec = (a, b) => {
-  return parseInt(b) - parseInt(a);
+  return b - a;
 };
 
 const MONKEY_LINES = 7;
 
 const newWorryLevel = (x, y, op) => {
-  if (y === "old") y = x;
-  if (op === "*") return x * y;
-  if (op === "+") return x + y;
+  if (y === "old") {
+    //console.log("before old assignment", y, x);
+    y = x;
+    //console.log("old assignment done", y, x);
+  }
+
+  if (op === "*") {
+    //console.log(x, "*", y, BigInt(x) * BigInt(y));
+    return BigInt(x) * BigInt(y);
+  }
+  if (op === "+") {
+    //console.log(x, "+", y, BigInt(x) + BigInt(y));
+    return BigInt(x) + BigInt(y);
+  }
 };
 
 const parseMonkey = (lines) => {
   const items = lines[1]
     .split(":")[1]
     .split(",")
-    .map((item) => parseInt(item));
+    .map((item) => BigInt(parseInt(item)));
   let [operator, operand] = lines[2].split(" ").slice(-2);
-  operand = isNaN(operand) ? "old" : parseInt(operand);
+  operand = isNaN(operand) ? "old" : BigInt(parseInt(operand));
 
-  const test = parseInt(lines[3].split(" ").slice(-1));
+  const test = BigInt(parseInt(lines[3].split(" ").slice(-1)));
   const throwTo = [parseInt(lines[4].slice(-1)), parseInt(lines[5].slice(-1))];
   return {
     items: items,
@@ -40,22 +51,34 @@ const parseMonkey = (lines) => {
 };
 
 const round = (monkeys, divide = true) => {
+  let newMonkeys = [...monkeys];
+  //console.log("newMonkeys", newMonkeys);
   for (let i = 0; i < monkeys.length; i++) {
-    const monkey = monkeys[i];
+    let monkey = newMonkeys[i];
     //console.log(monkey.items);
-    //console.log(monkey.inspections);
-    const itemsAndOwners = turn(monkey, divide);
+    // console.log("monkey ", i);
+    const { itemsAndOwners, inspections } = turn(monkey, divide);
     monkey.items = [];
+    //monkey = { ...monkey, items: [] };
     itemsAndOwners.forEach(({ worryLevel, newOwner }) => {
-      monkeys[newOwner].items.push(worryLevel);
+      newMonkeys[newOwner].items.push(worryLevel);
     });
+    monkey.inspections += inspections;
+    //newMonkeys.push(monkey);
+    //console.log(
+    //  `monkey ${i} made ${inspections} inspections, now has `,
+    //  monkey.inspections
+    //);
   }
+  return newMonkeys;
 };
 
 const turn = (monkey, divide = true) => {
   let itemsAndOwners = [];
+  let inspections = 0;
   for (const item in monkey.items) {
-    monkey.inspections++;
+    inspections++;
+    //console.log(monkey.throwTo);
     const { worryLevel, newOwner } = handleItem(
       monkey.items[item],
       monkey.operation,
@@ -65,14 +88,26 @@ const turn = (monkey, divide = true) => {
     );
     itemsAndOwners.push({ worryLevel, newOwner });
   }
-  return itemsAndOwners;
+  return { itemsAndOwners: itemsAndOwners, inspections: inspections };
 };
 
 const handleItem = (item, operation, test, throwTo, divide = true) => {
   const worryLevel = newWorryLevel(item, operation.operand, operation.operator);
-  const newLevel = divide ? Math.floor(worryLevel / 3) : worryLevel; // % test;
-  const newOwner = newLevel % test === 0 ? throwTo[0] : throwTo[1];
-  return { worryLevel: newLevel, newOwner: newOwner };
+  //console.log(worryLevel);
+  //const newLevel = divide
+  //  ? BigInt(Math.floor(worryLevel / 3n))
+  //  : BigInt(worryLevel); // % test;
+  console.log(
+    "BigInt(worryLevel)",
+    BigInt(worryLevel),
+    "BigInt(test)",
+    BigInt(test),
+    "BigInt(worryLevel) % BigInt(test)",
+    BigInt(worryLevel) % BigInt(test)
+  );
+  const newOwner =
+    BigInt(worryLevel) % BigInt(test) === 0 ? throwTo[0] : throwTo[1];
+  return { worryLevel: worryLevel, newOwner: newOwner };
 };
 
 const monkeyStartsAt = Array.from(
@@ -84,19 +119,33 @@ let monkeys = monkeyStartsAt.map((l) => parseMonkey(lines.slice(l, l + 6)));
 
 for (let i = 0; i < 20; i++) round(monkeys, true);
 
-console.log(
-  monkeys
-    .map((m) => m.inspections)
-    .sort(sortDec)
-    .slice(0, 2)
-    .reduce((acc, curr) => acc * curr, 1)
-);
+//console.log(
+//  "? ",
+//  monkeys
+//    .map((m) => m.inspections)
+//    .sort(sortDec)
+//    .slice(0, 2)
+//    .reduce((acc, curr) => acc * curr, 1)
+//);
 
 monkeys = monkeyStartsAt.map((l) => parseMonkey(lines.slice(l, l + 6)));
+console.log(monkeys.length);
 
-for (let i = 0; i < 1000; i++) round(monkeys, false);
+for (let i = 0; i < 20; i++) {
+  monkeys = round(monkeys, false);
 
-console.log(monkeys.map((m) => m.inspections).sort(sortDec));
+  //console.log(
+  //  i,
+  //  monkeys.map((m) => m.inspections),
+  //  monkeys.map((m) => m.items.length),
+  //  monkeys.reduce((acc, curr) => acc + curr.inspections, 0)
+  //);
+}
+
+console.log(
+  "inspections",
+  monkeys.map((m) => m.inspections)
+); //.sort(sortDec));
 
 //console.log(monkeys);
 
