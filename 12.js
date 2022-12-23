8c5fdc65c904d98e861051ee66a8be3c3419193c
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-12-23 11:50:08 +0200

    Day 12 pathfinding WIP

--- 12.js
+++ 12.js
@@ -1,9 +1,25 @@
 const f = require("fs");
 
+class Node {
+  constructor(g, h, parent) {
+    this.g = g;
+    this.h = h;
+    this.f = g + h;
+    this.parent = parent;
+    this.open = false;
+    this.closed = false;
+  }
+
+  update_g(g) {
+    this.g = g;
+    this.f = g + this.h;
+  }
+}
+
 let data = [];
 
 // read the data
-const dataRaw = f.readFileSync("12-data.txt", "utf-8");
+const dataRaw = f.readFileSync("12-data-mini.txt", "utf-8");
 dataRaw.split(/\r?\n/).forEach((line) => {
   data.push(line.split("").map((c) => c.charCodeAt(0) - 97));
 });
@@ -20,22 +36,54 @@ const TARGET_POS = [Math.floor(TARGET_IDX / WIDTH), TARGET_IDX % WIDTH];
 
 // console.log(START_POS, TARGET_POS);
 
-const generateVisitedArray = (width, height, startPos) => {
-  let visited = Array(height)
-    .fill()
-    .map((a) => [...Array(width)])
-    .map((a) => a.fill(0));
-  visited[startPos[0]][startPos[1]] = 1;
-  return visited;
+const generateNodes = (width, height, startPos) => {
+  //let nodes = Array(height)
+  //  .fill()
+  //  .map((a) => [...Array(width)])
+  //  .map((a) => a.fill(0));
+  //nodes[startPos[0]][startPos[1]] = 1;
+  //return nodes;
+  let nodes = [];
+  for (let iy = 0; iy < HEIGHT; iy++) {
+    let row = [];
+    for (let ix = 0; ix < WIDTH; ix++) {
+      const node = new Node(-1, -1);
+      row.push(node);
+    }
+    nodes.push(row);
+  }
+  nodes[startPos[0]][startPos[1]].open = true;
+  return nodes;
 };
 
-let visited = generateVisitedArray(WIDTH, HEIGHT, START_POS);
-console.log(visited.length, visited[0].length);
+// console.log(visited.length, visited[0].length);
 
-let pos = START_POS;
-
-const distToTarget = (pos, target) => {
+const manhattanDist = (pos, target) => {
   const dy = Math.abs(pos[0] - target[0]);
   const dx = Math.abs(pos[1] - target[1]);
   return dy + dx;
 };
+
+let visited = generateNodes(WIDTH, HEIGHT, START_POS);
+let pos = START_POS;
+
+//while (!(pos[0] === TARGET_POS[0] && pos[1]) === TARGET_POS[1]) {
+//  let direction = [0, 0];
+//  // right
+//
+//  // down
+//  // left
+//  // up
+//}
+
+//const n = new Node(10, 20);
+//console.log(n);
+//n.update_g(20);
+//console.log(n);
+//
+//const m = new Node(20, 20, n);
+//console.log(m);
+
+let nodes = generateNodes(WIDTH, HEIGHT, START_POS);
+
+console.log(nodes);
